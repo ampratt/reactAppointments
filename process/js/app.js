824682e0acffb9adb71bc9cba667ab8d4d62e9a8
Aaron Pratt <pratt.aaron.m@gmail.com>
2017-03-30 23:13:41 +0300

    added search component (input field and sort by button). handle events by passing target value,id to parent via props and then modifying state object in main component

--- process/js/app.js
+++ process/js/app.js
@@ -4,12 +4,15 @@ var _ = require('lodash');
 
 var AptList = require('./AptList');
 var AddAppointment = require('./AddAppointment');
-
+var SearchAppointments = require('./SearchAppointments');
 
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
     	aptBodyVisible: false, 
+      orderBy: 'petName',
+      orderDir: 'asc',
+      queryText: '',
       	myAppointments: []
     } //return
   }, //getInitialState
@@ -54,8 +57,47 @@ var MainInterface = React.createClass({
   	}); //setState
   }, //addItem
 
+  reOrder: function(orderBy, orderDir) {
+    this.setState({
+      orderBy: orderBy,
+      orderDir: orderDir
+    }); //setState
+  }, //reOrder
+
+  searchApts(q) {
+    // 1. receive query texts from subcomponent
+    // 2. set state
+    this.setState({
+      queryText: q
+    }); 
+  }, //searchApts
+
   render: function() {
-    var filteredApts = this.state.myAppointments;
+    var filteredApts = []; //this.state.myAppointments;
+    var orderBy = this.state.orderBy;
+    var orderDir = this.state.orderDir;
+    var queryText = this.state.queryText;
+    var myAppointments = this.state.myAppointments;
+
+
+    myAppointments.forEach(function(item) {
+      if(
+        (item.petName.toLowerCase().indexOf(queryText)!=-1) ||
+        (item.ownerName.toLowerCase().indexOf(queryText)!=-1) ||
+        (item.aptDate.toLowerCase().indexOf(queryText)!=-1) ||
+        (item.aptNotes.toLowerCase().indexOf(queryText)!=-1)
+      ) {
+        filteredApts.push(item);  //if any hits a match, push to filtered array
+      }
+    }); //forEach
+
+
+    // 1)what to order, 2)order by what 3) direction
+    filteredApts = _.orderBy(filteredApts, function(item) {
+      return item[orderBy].toLowerCase();
+    }, orderDir);//orderBy
+
+
     filteredApts = filteredApts.map(function(item, index) {
       return(
         <AptList key = {index}
@@ -70,6 +112,12 @@ var MainInterface = React.createClass({
 	          bodyVisible = { this.state.aptBodyVisible }
 	          handleToggle = { this.toggleAddDisplay }
 	          addApt = { this.addItem }
+        />
+        <SearchAppointments 
+          orderBy = { this.state.orderBy }
+          orderDir = { this.state.orderDir }
+          onReOrder = { this.reOrder }
+          onSearch = { this.searchApts }
         />
         <ul className="item-list media-list">{filteredApts}</ul>
       </div>
