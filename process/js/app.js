ca7cf56d20c7c2f514ffb8b7d9b9a32ee3334bc4
Aaron Pratt <pratt.aaron.m@gmail.com>
2017-03-30 16:27:10 +0300

    event handling, pass props from child to parent, modify state object, react auto rerenders

--- process/js/app.js
+++ process/js/app.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+var _ = require('lodash');
 var AptList = require('./AptList');
 
 var MainInterface = React.createClass({
@@ -24,12 +24,24 @@ var MainInterface = React.createClass({
     this.serverRequest.abort();
   },
 
+  //simply modify state object, which automatically re-renders
+  deleteMessage: function(item) {
+  	var allApts = this.state.myAppointments;
+  	//remove item from list of all apts
+  	var newApts = _.without(allApts, item);
+  	this.setState({
+			myAppointments: newApts
+  	}); // setstate
+  }, //delete message
+
   render: function() {
     var filteredApts = this.state.myAppointments;
     filteredApts = filteredApts.map(function(item, index) {
       return(
         <AptList key = {index}
-        		 singleItem = { item } />
+        		 singleItem = { item } 
+        		 whichItem = { item }	//stores current item clicked on
+        		 onDelete = { this.deleteMessage }/>	//executes delet funtion which changes state
       ) //return
     }.bind(this)); //filteredApts.map
     return (
